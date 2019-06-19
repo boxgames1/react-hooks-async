dbc1fe841054ff0a631fb5d8bf8cf961348d0e34
Oliverio Alonso <oliverioalonso@spotahome.com>
2019-06-19 23:52:14 +0200

    Updates

---
+++ precache-manifest.bfe48bf6633d3496be57e2aa04bcaf6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf94581a15db5136530b02ee985b88a5",
+    "url": "/react-hooks-async/index.html"
+  },
+  {
+    "revision": "e0d1d8763fb83d8d08ce",
+    "url": "/react-hooks-async/static/css/main.75716616.chunk.css"
+  },
+  {
+    "revision": "08fa4785042d1cae8680",
+    "url": "/react-hooks-async/static/js/2.405c50e5.chunk.js"
+  },
+  {
+    "revision": "e0d1d8763fb83d8d08ce",
+    "url": "/react-hooks-async/static/js/main.e6d72fea.chunk.js"
+  },
+  {
+    "revision": "6ae4fc47edfb3e6140bf",
+    "url": "/react-hooks-async/static/js/runtime~main.3b16c14d.js"
+  }
+]);
